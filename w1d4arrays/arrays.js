e976caeff38c201e84bba017a7abed166d7669e9
Hselam4 <selamawit.habte@miu.edu>
2021-09-09 16:02:16 -0500

    w2d2

--- w1d4arrays/arrays.js
+++ w1d4arrays/arrays.js
@@ -4,18 +4,13 @@ function maxOfThree(num1, num2, num3){
     let max=num1;
     if(max < num2){
         max = num2;
-    }
-    
+    }  
     if(max < num3){
         max = num3;
     }
-   
-
 
     return max;
-
 }
-
 
 function sum(arr){
     let sum = 0;
---
+++ w2d2Obj/objects.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+module.exports = { accumulator, Calculator }; //add all of your function names here that you need for the node mocha tests
+
+/**
+ * @param {Number} startingValue starting value to be accumulated
+ * @param {Number} increment increments by this number
+ * @returns {undefined}
+ */
+
+
+ functionAccumulator(startingValue, increment) 
+ {        
+ //implement this
+ this.currentValue = startingValue;
+ this.increment = increment;
+
+ this.accumulate = function () {        
+ this.currentValue += this.increment;
+ returnthis.currentValue;
+   }        ;
+
+ this.report = function () {        
+ returnthis.currentValue;
+   }        ;
+ }        
+let accumulator = new Accumulator(5, 10);
+accumulator.report();
+accumulator.report();
+console.log(accumulator.startingValue);
+
+/**
+ * @returns {Calculator} this is a constructor function
+ */
+function Calculator() {
+  this.aaa = 0;
+  this.bbb = 0;
+  this.setValues = function (aaa, bbb) {
+    this.aaa = aaa;
+    this.bbb = bbb;
+  };
+  this.sum = function () {
+    return this.aaa + this.bbb;
+  };
+  this.mul = function () {
+    return this.aaa * this.bbb;
+  };
+
+  //implement this
+}
